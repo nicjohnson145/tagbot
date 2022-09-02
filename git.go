29aa9547ce1ac6787c90f2e3cbb60ea626f4a9e6
Nic Johnson <nicjohnson145@hotmail.com>
2022-09-02 14:39:10 -0500

    feat: ability to list commits since the latest tag

--- git.go
+++ git.go
@@ -6,8 +6,12 @@ import (
 	"github.com/apex/log"
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/object"
 	"sort"
+	"errors"
 )
+
+var errStopIteration = errors.New("stop iteration")
 
 func NewGitRepo(path string) (*GitRepo, error) {
 	repo, err := git.PlainOpen(path)
@@ -24,13 +28,18 @@ type GitRepo struct {
 	repo *git.Repository
 }
 
-func (g *GitRepo) LatestTag() (*semver.Version, error) {
+type Tag struct {
+	Hash plumbing.Hash
+	Tag *semver.Version
+}
+
+func (g *GitRepo) LatestTag() (*Tag, error) {
 	iter, err := g.repo.Tags()
 	if err != nil {
 		return nil, fmt.Errorf("error getting repo tags: %w", err)
 	}
 
-	tags := []*semver.Version{}
+	tags := []Tag{}
 	err = iter.ForEach(func(tag *plumbing.Reference) error {
 		v, err := semver.NewVersion(tag.Name().Short())
 		if err != nil {
@@ -38,7 +47,10 @@ func (g *GitRepo) LatestTag() (*semver.Version, error) {
 			return nil
 		}
 
-		tags = append(tags, v)
+		tags = append(tags, Tag{
+			Tag: v,
+			Hash: tag.Hash(),
+		})
 		return nil
 	})
 	if err != nil {
@@ -50,6 +62,46 @@ func (g *GitRepo) LatestTag() (*semver.Version, error) {
 		return nil, nil
 	}
 
-	sort.Sort(semver.Collection(tags))
-	return tags[len(tags)-1], nil
+	sort.Slice(tags, func(i, j int) bool {
+		return tags[i].Tag.LessThan(tags[j].Tag)
+	})
+	return &tags[len(tags)-1], nil
+}
+
+func (g *GitRepo) CommitsSinceTag(tag *Tag) ([]string, error) {
+	iter, err := g.repo.Log(&git.LogOptions{
+		Order: git.LogOrderCommitterTime,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("error getting commit list: %v", err)
+	}
+
+	var appendAndContinue func(*object.Commit) bool
+	if tag == nil {
+		// If we don't get a tag, that just means we consider all commits forever
+		appendAndContinue = func(*object.Commit) bool { return true }
+	} else {
+		// Otherwise we stop once we hit the commit with the same hash as the tag
+		appendAndContinue = func(c *object.Commit) bool { return c.Hash != tag.Hash }
+	}
+
+	commits := []string{}
+	err = iter.ForEach(func(c *object.Commit) error {
+		if appendAndContinue(c) {
+			commits = append(commits, c.Message)
+			return nil
+		}
+		return errStopIteration
+	})
+	if err != nil && !errors.Is(err, errStopIteration) {
+		return nil, fmt.Errorf("error iterating commits: %w", err)
+	}
+
+	// Reverse it, so we can iterate in the order things were committed
+	reversed := make([]string, 0, len(commits))
+	for i := len(commits) - 1; i >= 0; i-- {
+		reversed = append(reversed, commits[i])
+	}
+
+	return reversed, nil
 }
--- git_test.go
+++ git_test.go
@@ -29,5 +29,34 @@ func TestLatestTag(t *testing.T) {
 	tag, err := g.LatestTag()
 	require.NoError(t, err, "getting latest tag")
 	require.NotNil(t, tag)
-	require.Equal(t, "v0.1.1", tag.Original())
+	require.Equal(t, "v0.1.1", tag.Tag.Original())
+}
+
+func TestCommitsSinceTag(t *testing.T) {
+	repo := makeTestRepo(
+		t,
+		testcommit{
+			Msg: "foobar1",
+			Tag: "v0.1.0",
+		},
+		testcommit{
+			Msg: "some commit 1",
+		},
+		testcommit{
+			Msg: "some commit 2",
+		},
+	)
+
+	g := GitRepo{
+		repo: repo,
+	}
+
+	tag, err := g.LatestTag()
+	require.NoError(t, err)
+	require.NotNil(t, tag)
+	require.Equal(t, "v0.1.0", tag.Tag.Original())
+
+	commitsSince, err := g.CommitsSinceTag(tag)
+	require.NoError(t, err)
+	require.Equal(t, []string{"some commit 1", "some commit 2"}, commitsSince)
 }
